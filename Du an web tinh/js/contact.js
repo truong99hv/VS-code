e26bbcee37b89a9393d7fcd29d6d71a531db9579
Hoàng Trường <congatruong99pro@gmail.com>
2023-02-15 15:11:11 +0700

    final

--- Du an web tinh/js/contact.js
+++ Du an web tinh/js/contact.js
@@ -1,3 +1,48 @@
+function filterObjects(c) {
+  let x, i;
+
+  x = document.getElementsByClassName("box-contact");
+  if (c == "all") {
+    c = "";
+  }
+
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) {
+      addClass(x[i], "show");
+    }
+  }
+}
+
+function addClass(element, name) {
+  let i, arr1, arr2;
+
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function removeClass(element, name) {
+  let i, arr1, arr2;
+
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+
+  element.className = arr1.join(" ");
+}
+
+// menu
 // Menu
 const menu = document.querySelector(".navbar__links");
 const menuButton = document.querySelector(".navbar__icons");
@@ -48,27 +93,6 @@ ScrollReveal().reveal(".about-us", { duration: 2000 });
 ScrollReveal().reveal(".about-us-img", { distance: "200px" });
 ScrollReveal().reveal(".about-us-img", { origin: "right" });
 ScrollReveal().reveal(".about-us-img", { duration: 2000 });
-
-// countdown
-let fuT = new Date("mar 1, 2023 00:00:00").getTime();
-
-setInterval(function () {
-  let noW = new Date().getTime();
-  let D = fuT - noW;
-  let days = Math.floor(D / (1000 * 60 * 60 * 24));
-  let hours = Math.floor(D / (1000 * 60 * 60));
-  let minutes = Math.floor(D / (1000 * 60));
-  let seconds = Math.floor(D / 1000);
-
-  hours %= 24;
-  minutes %= 60;
-  seconds %= 60;
-
-  document.getElementById("days").innerText = days;
-  document.getElementById("hours").innerText = hours;
-  document.getElementById("minutes").innerText = minutes;
-  document.getElementById("seconds").innerText = seconds;
-});
 
 ScrollReveal().reveal(".countdown-title", { distance: "200px" });
 ScrollReveal().reveal(".countdown-title", { origin: "left" });
@@ -170,3 +194,5 @@ ScrollReveal().reveal(".list-service", { duration: 2000 });
 ScrollReveal().reveal(".link-hair-style", { distance: "500px" });
 ScrollReveal().reveal(".link-hair-style", { origin: "right" });
 ScrollReveal().reveal(".link-hair-style", { duration: 2000 });
+
+ScrollReveal().reveal(".content-contact", { delay: 2000 });
--- Du an web tinh/js/project.js
+++ Du an web tinh/js/project.js
@@ -49,27 +49,6 @@ ScrollReveal().reveal(".about-us-img", { distance: "200px" });
 ScrollReveal().reveal(".about-us-img", { origin: "right" });
 ScrollReveal().reveal(".about-us-img", { duration: 2000 });
 
-// // countdown
-// let fuT = new Date("nov 11, 2022 00:00:00").getTime();
-
-// setInterval(function () {
-//   let noW = new Date().getTime();
-//   let D = fuT - noW;
-//   let days = Math.floor(D / (1000 * 60 * 60 * 24));
-//   let hours = Math.floor(D / (1000 * 60 * 60));
-//   let minutes = Math.floor(D / (1000 * 60));
-//   let seconds = Math.floor(D / 1000);
-
-//   hours %= 24;
-//   minutes %= 60;
-//   seconds %= 60;
-
-//   document.getElementById("days").innerText = days;
-//   document.getElementById("hours").innerText = hours;
-//   document.getElementById("minutes").innerText = minutes;
-//   document.getElementById("seconds").innerText = seconds;
-// });
-
 ScrollReveal().reveal(".countdown-title", { distance: "200px" });
 ScrollReveal().reveal(".countdown-title", { origin: "left" });
 ScrollReveal().reveal(".countdown-title", { duration: 2000 });
