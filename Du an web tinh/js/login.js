e7cf308508fa3c602811962784429836cbba56f8
Hoàng Trường <congatruong99pro@gmail.com>
2023-02-17 20:20:07 +0700

    ok

--- Du an web tinh/js/login.js
+++ Du an web tinh/js/login.js
@@ -1,123 +1,10 @@
-const menu = document.querySelector(".navbar__links");
-const menuButton = document.querySelector(".navbar__icons");
-const overlay = document.querySelector("#overlay");
+// ScrollReveal().reveal(".list-service", { distance: "500px" });
+// ScrollReveal().reveal(".list-service", { origin: "left" });
+// ScrollReveal().reveal(".list-service", { duration: 2000 });
 
-menuButton.addEventListener("click", () => {
-  menu.classList.toggle("navbar__open");
-  menuButton.classList.toggle("open");
-  overlay.classList.toggle("show");
-});
-
-overlay.addEventListener("click", () => {
-  menu.classList.toggle("navbar__open");
-  menuButton.classList.toggle("open");
-  overlay.classList.toggle("show");
-});
-
-const swiper = new Swiper(".swiper", {
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  loop: true,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-// ===Banner===
-ScrollReveal().reveal(".banner", { delay: 400 });
-ScrollReveal().reveal(".banner", { duration: 4000 });
-ScrollReveal().reveal(".banner", { opacity: 0.8 });
-ScrollReveal().reveal(".banner", { easing: "ease-in" });
-
-$(".list-service").slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  autoplay: true,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-  ],
-});
-
-$(".brand").slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  autoplay: true,
-  slidesToShow: 5,
-  slidesToScroll: 5,
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 5,
-      },
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-      },
-    },
-    {
-      breakpoint: 576,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-  ],
-});
-
-ScrollReveal().reveal(".list-service", { distance: "500px" });
-ScrollReveal().reveal(".list-service", { origin: "left" });
-ScrollReveal().reveal(".list-service", { duration: 2000 });
-
-ScrollReveal().reveal(".link-hair-style", { distance: "500px" });
-ScrollReveal().reveal(".link-hair-style", { origin: "right" });
-ScrollReveal().reveal(".link-hair-style", { duration: 2000 });
+// ScrollReveal().reveal(".link-hair-style", { distance: "500px" });
+// ScrollReveal().reveal(".link-hair-style", { origin: "right" });
+// ScrollReveal().reveal(".link-hair-style", { duration: 2000 });
 
 let username = document.querySelector("#username");
 let email = document.querySelector("#email");
@@ -208,7 +95,7 @@ form.addEventListener("submit", function (e) {
   ]);
 
   let isEmailError = checkEmailError(email);
-  let isUsernameLengthError = checkLengthError(username, 3, 15);
-  let isPassworkLengthError = checkLengthError(passwork, 3, 15);
+  let isUsernameLengthError = checkLengthError(username, 6, 15);
+  let isPassworkLengthError = checkLengthError(passwork, 6, 15);
   let isMatchPassworkError = checkMatchPassworkError(passwork, confirmPasswork);
 });
