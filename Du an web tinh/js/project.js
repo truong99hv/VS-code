2a1114cd73fc1992cace4f2c080296127d26e293
Hoàng Trường <congatruong99pro@gmail.com>
2023-02-18 08:04:25 +0700

    delete contact

--- Du an web tinh/js/project.js
+++ Du an web tinh/js/project.js
@@ -1,49 +1,49 @@
-// contact
-function filterObjects(c) {
-  let x, i;
+// // contact
+// function filterObjects(c) {
+//   let x, i;
 
-  x = document.getElementsByClassName("box-contact");
-  if (c == "all") {
-    c = "";
-  }
+//   x = document.getElementsByClassName("box-contact");
+//   if (c == "all") {
+//     c = "";
+//   }
 
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) {
-      addClass(x[i], "show");
-    }
-  }
-}
+//   for (i = 0; i < x.length; i++) {
+//     removeClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) {
+//       addClass(x[i], "show");
+//     }
+//   }
+// }
 
-function addClass(element, name) {
-  let i, arr1, arr2;
+// function addClass(element, name) {
+//   let i, arr1, arr2;
 
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
 
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) {
+//       element.className += " " + arr2[i];
+//     }
+//   }
+// }
 
-function removeClass(element, name) {
-  let i, arr1, arr2;
+// function removeClass(element, name) {
+//   let i, arr1, arr2;
 
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
 
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
 
-  element.className = arr1.join(" ");
-}
+//   element.className = arr1.join(" ");
+// }
 
-ScrollReveal().reveal(".content-contact", { delay: 2000 });
+// ScrollReveal().reveal(".content-contact", { delay: 2000 });
 
 // form
 const isValidEmail = (email) => {
